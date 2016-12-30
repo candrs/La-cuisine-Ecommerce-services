7c03124131a2e17322e53e936dfbe7c0367b01ed
Andres <andrx_s@hotmail.com>
2016-12-30 11:48:46 -0800

    La cuisine tabs service

---
+++ lcaicons/charmap.php
@@ -0,0 +1,12 @@
+<?php $icons = array();
+$icons['lcaicons']['lca-architect'] = array("class"=>'lca-architect',"tags"=>'lca-architect',"unicode"=>'');
+$icons['lcaicons']['lca-builders'] = array("class"=>'lca-builders',"tags"=>'lca-builders',"unicode"=>'');
+$icons['lcaicons']['lca-home'] = array("class"=>'lca-home',"tags"=>'lca-home',"unicode"=>'');
+$icons['lcaicons']['lca-international'] = array("class"=>'lca-international',"tags"=>'lca-international',"unicode"=>'');
+$icons['lcaicons']['lca-partners'] = array("class"=>'lca-partners',"tags"=>'lca-partners',"unicode"=>'');
+$icons['lcaicons']['lca-realtor'] = array("class"=>'lca-realtor',"tags"=>'lca-realtor',"unicode"=>'');
+$icons['lcaicons']['lca-support'] = array("class"=>'lca-support',"tags"=>'lca-support',"unicode"=>'');
+$icons['lcaicons']['lca-value'] = array("class"=>'lca-value',"tags"=>'lca-value',"unicode"=>'');
+$icons['lcaicons']['lca-mission'] = array("class"=>'lca-mission',"tags"=>'lca-mission',"unicode"=>'');
+$icons['lcaicons']['lca-retail'] = array("class"=>'lca-retail',"tags"=>'lca-retail',"unicode"=>'');
+$icons['lcaicons']['lca-history'] = array("class"=>'lca-history',"tags"=>'lca-history',"unicode"=>'');
